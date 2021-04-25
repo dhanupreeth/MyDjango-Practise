a0a5518875818a67ce5d8ce4d8540f3d9c23591f
Dhanesh Kumar <dhanupreeth@gmail.com>
2021-04-25 19:18:39 +0530

    firstapp views updated to print system date and time

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import datetime
 # Create your views here.
+
+def hello(request):
+    return HttpResponse('<h3><center> Welcome django Practise </center></h3>')
+
+def time(request):
+    time = datetime.datetime.now()
+    results = ('<h2> <center> System Time is :' +str(time) +'</center></h2>')
+    return HttpResponse(results)
