dfcd928af108e3fb7166418e984e27c4005f65a1
Dhanesh Kumar <dhanupreeth@gmail.com>
2021-04-25 19:48:19 +0530

    greeting message to the user

--- firstproject/templatesdemo/views.py
+++ firstproject/templatesdemo/views.py
@@ -5,7 +5,18 @@ import datetime
 
 def index(request):
     date = datetime.datetime.now()
+    message = 'Hi, '
+    hour = int(date.strftime('%H'))
+    if hour<12:
+        message+= 'Good Morning, Have a Nice Day'
+    else:
+        message+= 'Good Evening, Hope your day was Good'
+
     name = 'dhanesh'
-    current_date = {'display_date' :date, 'username' :name}
+    current_date = {
+        'display_date' :date, 
+        'username' :name,
+        'greetings' :message
+        }
     return render(request, 'templatesdemo/index.html', context=current_date)
     
