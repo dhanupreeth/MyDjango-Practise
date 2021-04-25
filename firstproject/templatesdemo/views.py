9c0d968dfb8c717d50a852388100ddac149a0cee
Dhanesh Kumar <dhanupreeth@gmail.com>
2021-04-25 19:39:06 +0530

    used templates tags to display name and date from dictionary

--- firstproject/templatesdemo/views.py
+++ firstproject/templatesdemo/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 # Create your views here.
 
 def index(request):
-    return render(request, 'templatesdemo/index.html')
+    date = datetime.datetime.now()
+    name = 'dhanesh'
+    current_date = {'display_date' :date, 'username' :name}
+    return render(request, 'templatesdemo/index.html', context=current_date)
     
